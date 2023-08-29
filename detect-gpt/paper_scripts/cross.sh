b651d1fe929389ef48d378063c9e76ccdec9472f
BrandonWherry <bwh1126@gmail.com>
2023-08-29 19:08:13 +0000

    all results

--- detect-gpt/paper_scripts/cross.sh
+++ detect-gpt/paper_scripts/cross.sh
@@ -1,16 +1,14 @@
 #/bin/bash
 
-python run.py --output_name llama2 --base_model_name Llama-2-13b-chat-hf       --scoring_model_name lmsys/vicuna-13b-v1.5-16k --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 200 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache
-python run.py --output_name llama2 --base_model_name Llama-2-13b-chat-hf       --scoring_model_name gpt2-xl                   --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 200 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache
-python run.py --output_name llama2 --base_model_name Llama-2-13b-chat-hf       --scoring_model_name Llama-2-13b-chat-hf       --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 200 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache
-
-python run.py --output_name gpt2   --base_model_name gpt2-xl                   --scoring_model_name lmsys/vicuna-13b-v1.5-16k --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 200 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache
-python run.py --output_name gpt2   --base_model_name gpt2-xl                   --scoring_model_name gpt2-xl                   --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 200 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache
-python run.py --output_name gpt2   --base_model_name gpt2-xl                   --scoring_model_name Llama-2-13b-chat-hf       --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 200 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache
-
-python run.py --output_name vicuna --base_model_name lmsys/vicuna-13b-v1.5-16k --scoring_model_name lmsys/vicuna-13b-v1.5-16k --mask_filling_model_name t5-3b --n_perturbation_list 10 --n_samples 200 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache
-python run.py --output_name vicuna --base_model_name lmsys/vicuna-13b-v1.5-16k --scoring_model_name gpt2-xl                   --mask_filling_model_name t5-3b --n_perturbation_list 10 --n_samples 200 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache
-python run.py --output_name vicuna --base_model_name lmsys/vicuna-13b-v1.5-16k --scoring_model_name Llama-2-13b-chat-hf       --mask_filling_model_name t5-3b --n_perturbation_list 10 --n_samples 200 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache
+python run.py --output_name llama2 --base_model_name Llama-2-13b-chat-hf       --scoring_model_name lmsys/vicuna-13b-v1.5-16k --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 100 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --batch_size 5
+python run.py --output_name llama2 --base_model_name Llama-2-13b-chat-hf       --scoring_model_name gpt2-xl                   --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 100 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --batch_size 5
+python run.py --output_name llama2 --base_model_name Llama-2-13b-chat-hf       --scoring_model_name Llama-2-13b-chat-hf       --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 100 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --batch_size 5
+# python run1.py --output_name gpt2   --base_model_name gpt2-xl                   --scoring_model_name lmsys/vicuna-13b-v1.5-16k --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 100 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --batch_size 5
+# python run1.py --output_name gpt2   --base_model_name gpt2-xl                   --scoring_model_name gpt2-xl                   --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 100 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --batch_size 5
+# python run1.py --output_name gpt2   --base_model_name gpt2-xl                   --scoring_model_name Llama-2-13b-chat-hf       --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 100 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --batch_size 5
+# python run2.py --output_name vicuna --base_model_name lmsys/vicuna-13b-v1.5-16k --scoring_model_name lmsys/vicuna-13b-v1.5-16k --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 100 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --batch_size 5
+# python run2.py --output_name vicuna --base_model_name lmsys/vicuna-13b-v1.5-16k --scoring_model_name gpt2-xl                   --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 100 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --batch_size 5
+# python run2.py --output_name vicuna --base_model_name lmsys/vicuna-13b-v1.5-16k --scoring_model_name Llama-2-13b-chat-hf       --mask_filling_model_name t5-3b --n_perturbation_list 50 --n_samples 100 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --batch_size 5
 
 
 # python run.py --output_name cross --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 10 --n_samples 2 --pct_words_masked 0.3 --span_length 2 --cache_dir ../.cache --chunk_size 1 --batch_size 1
